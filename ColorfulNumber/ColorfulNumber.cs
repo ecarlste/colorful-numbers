882e8e5c82344ee9bfbd7667e940f0d43fda76ae
Erik S. Carlsten <ecarlste@gmail.com>
2015-03-26 17:29:27 -0500

    Finished IsColor method with all unit tests passing

--- ColorfulNumber/ColorfulNumber.cs
+++ ColorfulNumber/ColorfulNumber.cs
@@ -12,10 +12,10 @@ namespace ColorfulNumberLibrary
         public Boolean IsColorful(long number)
         {
             string stringyNumber = number.ToString();
-            int digits = stringyNumber.Length;
+            int digitCount = stringyNumber.Length;
 
-            // if there are more than 10 digits in the number then it cannot be colorful
-            if (digits > 10)
+            // if there are more than 10 digitCount in the number then it cannot be colorful
+            if (digitCount > 10)
             {
                 return false;
             }
@@ -27,19 +27,42 @@ namespace ColorfulNumberLibrary
             }
 
             // split the string into 1 character elements and then convert the list to a HashSet of ints
-            List<char> test = new List<char>(stringyNumber.ToList());
-            HashSet<int> products = new HashSet<int>(test.ConvertAll<int>(x => (int)x));
+            List<char> digitsAsChar = new List<char>(stringyNumber.ToList());
+            List<int> digits = digitsAsChar.ConvertAll<int>(x => (int)Char.GetNumericValue(x));
+            HashSet<int> products = new HashSet<int>(digits);
 
             // before continuing we want to know that we found no duplicate products
             int productCount = products.Count;
-            if (digits != productCount)
+            if (digitCount != productCount)
             {
                 return false;
             }
 
+            // traverse the list using sublist sizes between 2 and n-1 where n is the size of lengh of the original number
+            for (int subListSize = 2; subListSize < digitCount; subListSize++)
+            {
+                // grab every sublist possible in order, of the size subListSize
+                for (int subListStart = 0; subListStart < digitCount - subListSize; subListStart++)
+                {
+                    // take the sublist and compute the product of all the digitCount together
+                    int product = 1;
+                    foreach (int digit in digits.GetRange(subListStart, subListSize))
+                    {
+                        product *= digit;
+                    }
 
+                    // once we compute the product insert it into the HashSet and check if the HashSet size has changed
+                    products.Add(product);
+                    if (products.Count == productCount)
+                    {
+                        return false;
+                    }
+                    // if we successfully added a product to our HashSet then increment productCount
+                    productCount++;
+                }
+            }
 
-            throw new NotImplementedException();
+            return true;
         }
     }
 }
