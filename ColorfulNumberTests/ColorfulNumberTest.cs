71b319076e5b2f6489536efafa9c076e8f757df9
Erik S. Carlsten <ecarlste@gmail.com>
2015-03-26 14:18:49 -0500

    Created ColorfulNumberLibrary and associated test project

---
+++ ColorfulNumberTests/ColorfulNumberTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ColorfulNumberLibraryTests
+{
+    [TestClass]
+    public class ColorfulNumberTest
+    {
+        [TestMethod]
+        public void TestIsColorful()
+        {
+        }
+    }
+}
